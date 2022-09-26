15c63edaa263d5c2e4fc84ded8b6ea71e4b2eab2
Syed Hasnain Zaidi <hasnainzaidi@hcsgroup.io>
2022-09-26 17:48:43 +0500

    simple todo app logic done

--- lib/todoApp/todoApp.dart
+++ lib/todoApp/todoApp.dart
@@ -8,8 +8,125 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+  var output = "";
+  List<dynamic> lst = [1, 2, 3, 4];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: lst.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 50,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 15),
+              child: ListTile(
+                title: Text("${lst[index]}"),
+                trailing: Container(
+                  width: 50,
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text("Edit Item"),
+                                    content: TextField(
+                                      onChanged: ((value) {
+                                        output = value;
+                                      }),
+                                    ),
+                                    actions: [
+                                      ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            setState(() {
+                                              lst.replaceRange(
+                                                  index, index + 1, {output});
+                                            });
+                                          },
+                                          child: Text("Update"))
+                                    ],
+                                  );
+                                });
+                          },
+                          child: Icon(Icons.edit)),
+                      GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              lst.removeAt(index);
+                            });
+                          },
+                          child: Icon(Icons.delete)),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("Warrning"),
+                        content:
+                            Text("Are you sure you want to delet all items"),
+                        actions: [
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  lst.clear();
+                                });
+                              },
+                              child: Text("Yes"))
+                        ],
+                      );
+                    });
+              },
+              child: Icon(Icons.delete_forever_sharp),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("Add Item"),
+                        content: TextField(
+                          onChanged: ((value) {
+                            output = value;
+                          }),
+                        ),
+                        actions: [
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  lst.add(output);
+                                });
+                              },
+                              child: Text("Add"))
+                        ],
+                      );
+                    });
+              },
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
